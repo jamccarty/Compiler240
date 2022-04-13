f28759c9c0ae69400544130326513d0027768235
Grace Choe <gchoe@goldengate.cs.brynmawr.edu>
2022-04-13 14:35:12 -0400

    grace is fixing rn

--- compile.c
+++ compile.c
@@ -5,11 +5,18 @@
 
 char** return_var_array(char *line, int linenum){
   char *whitespace = " \t\f\r\v\n";
-  char **array = malloc(sizeof(char *) * 50);
-
+  //char **array = malloc(sizeof(char *) * 50);
+  char **array = (char**)calloc(50, sizeof(char));
+  
   for(int i = 0; i < 50; i++){
-    array[i] = malloc(sizeof(char) * 150);
-    memset(array[i], (int)'\0', 149);
+    //array[i] = malloc(sizeof(char) * 150);
+    array[i] = (char*)calloc(150, sizeof(char));
+    //memset(array[i], (int)'\0', 149);
+  }
+
+  if (array == NULL) {
+    printf("ERROR: calloc failed!\n");
+    exit(1);
   }
 
   //why are we tokening again if the previous token was at the word we need to
@@ -32,25 +39,23 @@ char** return_var_array(char *line, int linenum){
     numvars++;
     token = strtok(NULL, whitespace);
 
-    if(token == NULL){ //shouldn't this be if token != "int" since it's not just NULL?
+    if(strtok(token, "int") != 0) {
       printf("Error: Formatting error on line %d\n", linenum);
       exit(1);
     }
 
-    //need to token again to get the real name of the variable to put in array,
-    //I think?
+    token = strtok(NULL, whitespace);
     strcpy(array[numvars], token);
     token = strtok(NULL, whitespace);
 
-    /*if(token == NULL){
-      printf("Formatting error on line %d - ';' expected\n", linenum);
-      exit(1);
-    }*/
-
-    ////
     if (strcmp(token, ")") == 0) {
       break;
+    } else if (token == NULL) {
+      printf("Formatting error on line %d - ';' expected\n", linenum);
+      exit(1);
     }
+
+    token = strtok(NULL, whitespace);
   }
   
   token = strtok(NULL, whitespace);
@@ -100,59 +105,68 @@ char** parse_function_header(char* line, int linenum){
 }
 
 char** parse_line_programs(FILE *file){
-    char *whitespace = " \t\f\r\v\n";
-    char *line = NULL;
-    char **all_vars = malloc(sizeof(char*) * 150);
-    ssize_t read;
-    ssize_t length = 0;
-    int linenum = 0;
+  char *whitespace = " \t\f\r\v\n";
+  char *line = NULL;
+  char **all_vars = malloc(sizeof(char*) * 150);
+  ssize_t read;
+  ssize_t length = 0;
+  int linenum = 0;
 
-    while(read = getline(&line, &length, file) != -1){
-        char **line_vars;
-        linenum++;
-        line_vars = parse_function_header(line, linenum);
-        if(line_vars == NULL){
-          char *token = strtok(line, whitespace);
-          if(token == NULL){
-            printf("ERROR\n");
-            exit(1);
-          }
-          if(strcmp(token, "int") == 0){
-            line_vars = return_var_array(line, linenum);
-          }
-        }
+  while(read = getline(&line, &length, file) != -1){
+    char **line_vars;
+    linenum++;
+    line_vars = parse_function_header(line, linenum);
+    
+    if(line_vars == NULL){
+      char *token = strtok(line, whitespace);
+  
+      if(token == NULL){
+        printf("Error\n");
+        exit(1);
+      }
+
+      if(strcmp(token, "int") == 0){
+        line_vars = return_var_array(line, linenum);
+      }
     }
+  }
+  
+  return all_vars;
 }
 
 int main(int argc, char** argv){
-    int enough_braces;
-    if(argc != 2){
-        printf("ERROR: no file inputted\n");
-        exit(1);
-    }
+  int enough_braces;
 
-    int len = strlen(argv[1]);
-    char *compiled_name = malloc(sizeof(char) * (len + 3));
+  if(argc != 2) {
+    printf("ERROR: no file inputted\n");
+    exit(1);
+  }
+
+  int len = strlen(argv[1]);
+  char *compiled_name = malloc(sizeof(char) * (len + 3));
     //memset(compiled_name, 0, len + 4);
-    strcpy(compiled_name, argv[1]);
-    strtok(compiled_name, ".");
-    strcat(compiled_name, ".asm");
+  strcpy(compiled_name, argv[1]);
+  strtok(compiled_name, ".");
+  strcat(compiled_name, ".asm");
 
-    FILE *og_file = fopen(argv[1], "r");
-    enough_braces = check_braces(og_file, argv[1]);
-    FILE *comp_file = fopen(compiled_name, "w");
+  FILE *og_file = fopen(argv[1], "r");
+  enough_braces = check_braces(og_file, argv[1]);
+  FILE *comp_file = fopen(compiled_name, "w");
 
 
-    if(og_file == NULL){
-        printf("ERROR: could not find file %s\n", argv[1]);
-        exit(1);
-    }
-    if(comp_file == NULL){
-        printf("ERROR: could not compile file\n");
-        exit(1);
-    }
-    if(enough_braces == 1){
-        exit(1);
-    }
-    return 0;
+  if(og_file == NULL) {
+    printf("Eror: could not find file %s\n", argv[1]);
+    exit(1);
+  }
+  
+  if(comp_file == NULL) {
+    printf("Error: could not compile file\n");
+    exit(1);
+  }
+  
+  if(enough_braces == 1) {
+    exit(1);
+  }
+  
+  return 0;
 }
